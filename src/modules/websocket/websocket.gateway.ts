340a8548d1109ea93e579d81d8927168195e4a89
layors4978 <layors4978@gmail.com>
2023-06-29 15:22:42 +0800

    feat(websocket): add echoHandler

--- src/modules/websocket/websocket.gateway.ts
+++ src/modules/websocket/websocket.gateway.ts
@@ -1,4 +1,14 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import {
+  WebSocketGateway,
+  SubscribeMessage,
+  MessageBody,
+} from '@nestjs/websockets';
 
 @WebSocketGateway()
-export class WebsocketGateway {}
+export class WebsocketGateway {
+  // use SubscribeMessage decorator to listen messages from client
+  @SubscribeMessage('echo')
+  echoHandler(@MessageBody() message: string): string {
+    return message;
+  }
+}
