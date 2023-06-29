65a81ff48aafd65ff0767314934458bf95522362
layors4978 <layors4978@gmail.com>
2023-06-29 14:44:43 +0800

    feat(websocket): add websocket gateway

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { WebsocketModule } from './modules/websocket/websocket.module';
 
 @Module({
-  imports: [],
+  imports: [WebsocketModule],
   controllers: [],
   providers: [],
 })
