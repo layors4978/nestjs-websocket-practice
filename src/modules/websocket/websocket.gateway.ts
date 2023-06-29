00404dad0fb82b1cc5d46944e9967f019976b049
layors4978 <layors4978@gmail.com>
2023-06-29 15:40:55 +0800

    feat(websocket): add broadcastHandler

--- src/modules/websocket/websocket.gateway.ts
+++ src/modules/websocket/websocket.gateway.ts
@@ -1,14 +1,24 @@
 import {
   WebSocketGateway,
+  WebSocketServer,
   SubscribeMessage,
   MessageBody,
 } from '@nestjs/websockets';
+import { Server } from 'socket.io';
 
 @WebSocketGateway({ cors: '*' })
 export class WebsocketGateway {
+  @WebSocketServer()
+  server: Server;
+
   // use SubscribeMessage decorator to listen messages from client
   @SubscribeMessage('echo')
   echoHandler(@MessageBody() message: string): string {
     return message;
+  }
+
+  @SubscribeMessage('broadcast')
+  broadcastHandler(@MessageBody() message: string): void {
+    this.server.emit('broadcast', message);
   }
 }
